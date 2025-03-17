559d92ccfa5f6693fcca7d7b128424be192a1f1d
mikepopo <haom27935@gmail.com>
2025-03-17 19:25:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -56,8 +56,8 @@ ico
   CAN_COPY: process.env.NEXT_PUBLIC_CAN_COPY || true, // 是否允许复制页面内容 默认允许，如果设置为false、则全栈禁止复制内容。
 
   // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
-  LAYOUT_SIDEBAR_REVERSE:
-    process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || true,
+  LAYOUT_SIDEBAR_REVERSE: true,
+
 
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
